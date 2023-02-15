78e51db724184f87a2fc81d1afd965c1bec59151
pathan <azizkhan.cool@gmail.com>
2023-02-15 12:06:43 +0530

    Update attributes.tf

--- Section 2 - Read, Generate, Modify Congiruations/attributes.tf
+++ Section 2 - Read, Generate, Modify Congiruations/attributes.tf
@@ -1,7 +1,12 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-east-1"
   access_key = "PUT-YOUR-ACCESS-KEY-HERE"
   secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
 }
 
 resource "aws_eip" "lb" {
@@ -9,11 +14,11 @@ resource "aws_eip" "lb" {
 }
 
 output "eip" {
-  value = aws_eip.lb
+  value = aws_eip.lb.public_ip
 }
 
 resource "aws_s3_bucket" "mys3" {
-  bucket = "kplabs-attribute-demo-001"
+  bucket = "aziz-attribute-demo-001"
 }
 
 output "mys3bucket" {
